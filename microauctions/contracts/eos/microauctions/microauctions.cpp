c2e630ca73d0de11705b006f39989794308930db
Tal Muskal <tal@bancor.network>
2019-02-01 13:37:06 +0000

    init

---
+++ microauctions/contracts/eos/microauctions/microauctions.cpp
@@ -0,0 +1,239 @@
+#include <math.h>
+
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/transaction.hpp>
+#include <eosiolib/asset.hpp>
+#include <eosiolib/symbol.hpp>
+#include <eosiolib/singleton.hpp>
+#include <eosiolib/action.hpp>
+
+
+using namespace eosio;
+using std::string;
+using std::vector;
+
+TABLE account_row {
+   name         account;    //account name
+   uint8_t      score;      //enables a score between 0 to 255
+   string       metadata;   //json meta data
+   time_point_sec   timestamp;
+
+   uint64_t primary_key()const { return account.value; }
+   uint64_t by_timestamp() const { return uint64_t(timestamp.sec_since_epoch()); }
+};
+typedef eosio::multi_index<
+  name("accounts"), account_row,
+  indexed_by<name("bytime"), const_mem_fun<account_row, uint64_t, &account_row::by_timestamp>>
+> accounts_score_table;
+
+
+CONTRACT microauctions : public eosio::contract {
+    using contract::contract;
+    public:
+
+        TABLE settings {
+            name            whitelist;
+            uint64_t        cycles;
+            uint64_t        seconds_per_cycle;
+            uint64_t        start_ts;
+            extended_asset  quantity_per_day;
+            extended_asset  accepted_token;
+        };
+        
+        
+        
+        
+        TABLE cycle {
+          asset quantity;
+          uint64_t number;
+          uint64_t primary_key()const { return number; }
+        };
+        
+        
+        
+        
+        TABLE account {
+          std::vector<cycle> amounts_cycles;
+        };
+
+
+        typedef eosio::singleton<"settings"_n, settings> settings_t;
+        typedef eosio::singleton<"account"_n, account> accounts_t;
+        typedef eosio::multi_index<"cycle"_n, cycle> cycles_t;
+        
+        
+        ACTION init(settings setting){
+          require_auth(_self);
+          settings_t settings_table(_self, _self.value);
+          // eosio_assert(!settings_table.exists(),"already inited");
+          settings_table.set(setting, _self);
+        }
+        
+        ACTION enroll(name from){
+          accounts_t accounts_table(_self, from.value);
+          settings_t settings_table(_self, _self.value);
+          auto current_settings = settings_table.get();
+          
+          auto current_account = accounts_table.get_or_create(from);
+          auto amounts_cycles = current_account.amounts_cycles;
+          uint64_t cycle_number = getCurrentCycle();
+          eosio_assert(cycle_number < current_settings.cycles, "auction ended");
+          eosio_assert(isWhitelisted(from), "whitelisting required");
+
+          for (int i = 0; i < amounts_cycles.size(); i++) 
+            if(current_account.amounts_cycles[i].number == cycle_number)
+              return;
+          
+          
+          cycle amount_cycle;  
+          amount_cycle.number = cycle_number;
+          asset quantity;
+          quantity.symbol = current_settings.accepted_token.quantity.symbol;
+          quantity.amount = 0;
+          amount_cycle.quantity = quantity;
+          current_account.amounts_cycles.insert(current_account.amounts_cycles.end(), amount_cycle);
+          accounts_table.set(current_account, from);
+          
+        }
+        
+        ACTION claim(name to){
+          // anyone can claim for a user
+          accounts_t accounts_table(_self, to.value);
+          settings_t settings_table(_self, _self.value);
+          auto current_settings = settings_table.get();
+          auto existing = accounts_table.get();
+          uint64_t cycle_number = getCurrentCycle();
+          
+          // foreach: days before current cycle
+          auto amounts_cycles = existing.amounts_cycles;
+          auto quantity_per_day = current_settings.quantity_per_day;
+          double total = 0;
+          for (int i = 0; i < amounts_cycles.size(); i++) {
+            auto current_cycle = amounts_cycles[i];
+            if(cycle_number <= current_cycle.number) // cycle not complete
+              continue; 
+            
+            cycles_t cycles_table(_self, _self.value);
+            auto cycle_entry = cycles_table.find(current_cycle.number);
+            
+            auto account_quantity = current_cycle.quantity;
+            
+            // calculate daily price
+            if(account_quantity.amount == 0)
+              continue;
+            double token_price = (double)quantity_per_day.quantity.amount / cycle_entry->quantity.amount;
+            total += (double)account_quantity.amount / token_price;
+            // calculate tokens
+            
+          }
+          if(total >= 1){
+            extended_asset tokens;
+            tokens.contract = quantity_per_day.contract;
+            tokens.quantity.amount = total;
+            tokens.quantity.symbol = quantity_per_day.quantity.symbol;
+            issueToken(to, tokens);
+          }
+          // remove all
+          existing.amounts_cycles = std::vector<cycle>();
+          accounts_table.set(existing, to);
+        }
+        
+
+        void transfer(name from, name to, asset quantity, string memo){
+          if(from == _self)
+            return;
+          require_auth(from);
+          accounts_t accounts_table(_self, from.value);
+          settings_t settings_table(_self, _self.value);
+          auto current_settings = settings_table.get();
+          
+          // calculate current cycle
+          uint64_t cycle_number = getCurrentCycle();
+          eosio_assert(cycle_number < current_settings.cycles, "auction ended");
+          eosio_assert(isWhitelisted(from), "whitelisting required");
+          eosio_assert(quantity.symbol == current_settings.accepted_token.quantity.symbol, "wrong asset symbol");
+          eosio_assert(_code == current_settings.accepted_token.contract, "wrong asset contract");
+          // TODO: parse memo to support different account than the sending account
+          // TODO: check minimum
+          increaseCycleAmountAccount(cycle_number, from, quantity);
+        }
+    private:
+      uint64_t getCurrentCycle(){
+        settings_t settings_table(_self, _self.value);
+        auto current_settings = settings_table.get();
+        auto elapsed_time = current_time() - current_settings.start_ts;
+        return elapsed_time / (current_settings.seconds_per_cycle * 1000000 );
+      }
+      
+      void increaseCycleAmountAccount(uint64_t cycle_number, name from, asset quantity){
+        accounts_t accounts_table(_self, from.value);
+        cycles_t cycles_table(_self, _self.value);
+        auto current_cycle_entry = cycles_table.find(cycle_number);
+        if(current_cycle_entry == cycles_table.end()){
+          cycles_table.emplace(_self, [&](auto &s) {
+            s.number = cycle_number;
+            s.quantity = quantity;
+          });
+        }
+        else{
+          cycles_table.modify(current_cycle_entry, _self, [&](auto &s) {
+            s.quantity += quantity;
+          });
+        }
+        
+        account current_account;
+        // if(accounts_table.exists())
+        current_account = accounts_table.get();
+        
+        bool found = false;
+        for (int i = 0; i < current_account.amounts_cycles.size(); i++) {
+          if(current_account.amounts_cycles[i].number != cycle_number)
+            continue;
+          
+          current_account.amounts_cycles[i].quantity += quantity;
+          found = true;
+          break;
+        }
+        eosio_assert(found,"must enroll first");
+        accounts_table.set(current_account, eosio::same_payer);
+        
+      }
+      bool isWhitelisted(name from){
+        settings_t settings_table(_self, _self.value);
+        auto current_settings = settings_table.get();
+        auto whitelist = current_settings.whitelist;
+        if(whitelist == _self)
+          return true;
+        accounts_score_table accounts_scores(current_settings.whitelist, current_settings.whitelist.value);
+        auto existing = accounts_scores.find(from.value);
+        if(existing != accounts_scores.end()){
+          if(existing->score > 70 )
+            return true;
+        }
+        
+        return false;
+        
+        
+      }
+      void issueToken(name to, extended_asset quantity){
+        action(permission_level{_self, "active"_n},
+           quantity.contract, "issue"_n,
+           std::make_tuple(to, quantity.quantity, std::string("DAPP token auction")))
+        .send();
+      }
+};
+
+extern "C" {
+  [[noreturn]] void apply(uint64_t receiver, uint64_t code, uint64_t action) {
+    if (action == "transfer"_n.value && code != receiver) {
+      eosio::execute_action(eosio::name(receiver), eosio::name(code),
+                            &microauctions::transfer);
+    }
+    if (code == receiver) {
+      switch (action) {
+        EOSIO_DISPATCH_HELPER(microauctions, (init)(enroll)(claim))
+      }
+    }
+    eosio_exit(0);
+  }
+}
---
+++ microauctions/test/microauctions.spec.js
@@ -0,0 +1,219 @@
+require("babel-core/register");
+require("babel-polyfill");
+import { assert } from 'chai';
+import 'mocha';
+
+const artifacts = require('../extensions/tools/eos/artifacts');
+const deployer = require('../extensions/tools/eos/deployer');
+const getDefaultArgs = require('../extensions/helpers/getDefaultArgs');
+const { getEos,getCreateAccount } = require('../extensions/tools/eos/utils');
+const delay = ms => new Promise(res => setTimeout(res, ms))
+
+var args = getDefaultArgs();
+var systemToken = (args.creator !== 'eosio') ? "EOS" : "SYS";
+
+async function genAllocateEOSTokens(account) {
+    const keys = await getCreateAccount(account, args);
+    const { creator } = args;
+    var eos = await getEos(creator, args);
+    let servicesTokenContract = await eos.contract('eosio.token');
+
+    await servicesTokenContract.issue({
+        to: account,
+        quantity: `1000.0000 ${systemToken}`,
+        memo: ""
+    }, {
+        authorization: `eosio@active`,
+        broadcast: true,
+        sign: true
+    });
+}
+
+const contractCode ='microauctions';
+var contractArtifact = artifacts.require(`./${contractCode}/`);
+var tokenContract = artifacts.require(`./Token/`);
+describe(`${contractCode} Contract`, () => {
+    var testcontract;
+    var disttokenContract;
+    const perDay = "100.0000"
+    const code = 'auction1';
+    const cycleTime = 5;
+    const distokenSymbol = "NEW"
+    const disttoken = 'distoken';
+    const testuser1 = "testuser1";
+    const testuser2 = "testuser2";
+    const testuser3 = "testuser3";
+     before( done => {
+        (async() => {
+            try {
+                
+                var deployedContract = await deployer.deploy(contractArtifact, code);
+                var deployedToken = await deployer.deploy(tokenContract, disttoken);
+                disttokenContract = await deployedToken.eos.contract(disttoken);
+
+                
+                var eos  = deployedContract.eos;
+                await genAllocateEOSTokens(testuser1);
+                await genAllocateEOSTokens(testuser2);
+                await genAllocateEOSTokens(testuser3);
+                testcontract = await eos.contract(code);  
+                
+                await disttokenContract.create(code,`10000000000.0000 ${distokenSymbol}`, {authorization: `${disttoken}@active`,
+                    broadcast: true,
+                    sign: true});
+                // var systemtokenContract = await eos.contract('eosio.token');
+                
+                var res = await testcontract.init({
+                    setting:{
+                        whitelist:code,
+                        cycles:50,
+                        seconds_per_cycle:cycleTime,
+                        start_ts: new Date().getTime()*1000,
+                        quantity_per_day:{
+                            contract: disttoken,
+                            amount:perDay,
+                            precision: 4,
+                            symbol: distokenSymbol
+                        },
+                        accepted_token:{
+                            contract: "eosio.token",
+                            amount: `0.0000`,
+                            precision: 4,
+                            symbol: systemToken
+                        }
+                    }
+                }, {authorization: `${code}@active`,
+                    broadcast: true,
+                    sign: true});
+                
+                
+                
+                done();
+                
+                
+            }
+            catch (e) {
+                done(e);
+            }                    
+        })();
+    });
+    
+    const _selfopts = {
+        authorization:[`${code}@active`]
+    };
+    
+    const claim = async(testuser1)=>{
+        var eos = await getEos(testuser1, args);
+        var testcontract1 = await eos.contract(code);
+        var res = await testcontract1.claim({
+            to:testuser1
+        }, {
+            authorization: `${testuser1}@active`,
+            broadcast: true,
+            sign: true
+        });
+        return res.processed.action_traces[0].inline_traces[0].act.data.quantity;
+    }
+    
+    const buy = async(testuser, quantity) =>{
+        var eos = await getEos(testuser, args);
+        var systemtokenContract = await eos.contract('eosio.token');
+        var testcontract1 = await eos.contract(code);
+        await testcontract1.enroll({
+                    from: testuser,
+                }, {
+                    authorization: `${testuser}@active`,
+                    broadcast: true,
+                    sign: true
+                });
+                
+                await systemtokenContract.transfer({
+                    from: testuser1,
+                    to: code,
+                    quantity: `${quantity} ${systemToken}`,
+                    memo:""
+                }, {
+                    authorization: `${testuser}@active`,
+                    broadcast: true,
+                    sign: true
+                });
+    }
+    const sleepCycle = ()=>{
+        return delay(cycleTime * 1000);
+    }
+    
+    it('one cycle auction', done => {
+        (async() => {
+            try {
+                await sleepCycle();
+                await buy(testuser1,"10.0000");
+                await sleepCycle();
+                var claim1 = await claim(testuser1);
+                assert.equal(claim1, "100.0000 NEW", "wrong claim amount");
+                // user1 should have 100.0000
+                done();
+            }
+            catch (e) {
+                done(e);
+            }                    
+        })();
+    });
+    it('two cycle auction', done => {
+        (async() => {
+            try {
+                await sleepCycle();
+                await buy(testuser1,"10.0000");
+                await sleepCycle();
+                await buy(testuser1,"10.0000");
+                await sleepCycle();
+                var claim1 = await claim(testuser1);
+                assert.equal(claim1, "200.0000 NEW", "wrong claim amount");
+                done();
+            }
+            catch (e) {
+                done(e);
+            }                    
+        })();
+    });
+    
+    it('one cycle auction - multiple users', done => {
+        (async() => {
+            try {
+                await buy(testuser1,"10.0000");
+                await buy(testuser2,"30.0000");
+                await sleepCycle();
+                var claim1 = await claim(testuser1);
+                var claim2 = await claim(testuser2);
+                assert.equal(claim1, "25.0000 NEW", "wrong claim amount");
+                assert.equal(claim2, "75.0000 NEW", "wrong claim amount");
+                done();
+            }
+            catch (e) {
+                done(e);
+            }                    
+        })();
+    });
+    it('two cycle auction - multiple users', done => {
+        (async() => {
+            try {
+                await sleepCycle();
+                await buy(testuser1,"10.0000");
+                await buy(testuser2,"30.0000");
+                await sleepCycle();
+                await buy(testuser1,"30.0000");
+                await buy(testuser2,"10.0000");
+                await sleepCycle();
+                var claim1 = await claim(testuser1);
+                var claim2 = await claim(testuser2);
+                assert.equal(claim1, "100.0000 NEW", "wrong claim amount");
+                assert.equal(claim2, "100.0000 NEW", "wrong claim amount");
+                done();
+            }
+            catch (e) {
+                done(e);
+            }                    
+        })();
+    });
+    
+    
+});
